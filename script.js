baf56a69c8bbe2d93d60144cdc92b0c9f97078b3
LuisCasa <luis_hp66@hotmail.com>
2023-08-22 17:04:16 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  const img = document.querySelector("#avatar img")
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "../assets/Avatar/eu2.jpeg")
+  } else {
+    img.setAttribute("src", "../assets/Avatar/eu4.jpg")
+  }
+}
